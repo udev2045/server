e5ca7efdd05da0210064bc69573435c27e1f77e1
Iv Karavaiev <ddev4620@gmail.com>
2023-09-14 00:48:21 +0300

    Part 3.2

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ let persons = [
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+app.get('/info', (request, response) => {
+    let date = new Date()
+    response.send(`Phonebook has info for ${persons.length} people<br>${date}`)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
