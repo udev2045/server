c7521a685a2e4c024cbbef337d867f5fd68dd307
Iv Karavaiev <ddev4620@gmail.com>
2023-09-15 00:03:13 +0300

    Part 3.3-3.4

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 const express = require('express')
 const app = express()
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+let persons = [{
+    "id": 1, "name": "Arto Hellas", "number": "040-123456"
+}, {
+    "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523"
+}, {
+    "id": 3, "name": "Dan Abramov", "number": "12-43-234345"
+}, {
+    "id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122"
+}]
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
 app.get('/info', (request, response) => {
     let date = new Date()
     response.send(`Phonebook has info for ${persons.length} people<br>${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    console.log(response)
+    const p = persons.find(person => person.id === id)
+    if (p) {
+        response.json(p)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
